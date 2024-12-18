5d4da03351c5b2e3042d54eb8165c289e6353a6d
Large-Mac <bighaodwofad@gmail.com>
2024-12-17 22:53:36 -0800

    Bare Nix Flake

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "My Portable Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      user = "neo";
+    in {
+      homeConfigurations.${user} = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        modules = [
+          {
+            home = {
+              username = user;
+              homeDirectory = "/home/${user}";
+              packages = with pkgs; [
+                # Your packages here
+                firefox
+                git
+                neovim
+                ripgrep
+              ];
+
+              stateVersion = "24.11";
+            };
+
+            programs.home-manager.enable = true;
+          }
+        ];
+      };
+    };
+}
