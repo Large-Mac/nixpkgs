5fd264964e889e83554ffe148b7d2cb96e7dd967
Large-Mac <bighaodwofad@gmail.com>
2024-12-25 00:16:59 -0800

    added ytdlp alias in bash

--- home/programs/shell.nix
+++ home/programs/shell.nix
@@ -6,7 +6,8 @@
         ll = "ls -l";
         update = "home-manager switch --flake .#neo";
         doom = "~/.config/emacs/bin/doom";
-      };
+        yta = "yt-dlp -f 'ba' -x --embed-thumbnail";
+        ytv = 'yt-dlp -f "bestvideo+bestaudio/best"';
     };
 
     direnv = {
