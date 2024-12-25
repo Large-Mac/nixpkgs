3c9c728cb405e9f2f57d85bc746ed50d9a16dbd4
Large-Mac <bighaodwofad@gmail.com>
2024-12-25 00:22:06 -0800

    fixed previous commit adding ytdlp aliases

--- home/programs/shell.nix
+++ home/programs/shell.nix
@@ -7,9 +7,9 @@
         update = "home-manager switch --flake .#neo";
         doom = "~/.config/emacs/bin/doom";
         yta = "yt-dlp -f 'ba' -x --embed-thumbnail";
-        ytv = 'yt-dlp -f "bestvideo+bestaudio/best"';
+        ytv = "yt-dlp -f 'bestvideo+bestaudio/best'";
     };
-
+  };
     direnv = {
       enable = true;
       nix-direnv.enable = true;
